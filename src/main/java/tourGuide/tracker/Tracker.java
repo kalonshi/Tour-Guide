02b52aa6479e14f6053409a3eb8250698d48e764
Patrice LASSEY <splassey@gmail.com>
2022-06-02 10:51:20 +0200

    tracker updated

---
+++ src/main/java/tourGuide/tracker/Tracker.java
@@ -0,0 +1,84 @@
+package tourGuide.tracker;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tourGuide.service.TourGuideService;
+import tourGuide.user.User;
+
+public class Tracker extends Thread {
+	private Logger logger = LoggerFactory.getLogger(Tracker.class);
+	private final TourGuideService tourGuideService;
+	private final Map<User, Boolean> completedTrackingMap = new HashMap<>();
+	private static final long trackingPollingInterval = TimeUnit.SECONDS.toSeconds(5);
+	private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	private boolean stop = false;
+
+	public Tracker(TourGuideService tourGuideService) {
+		this.tourGuideService = tourGuideService;
+
+		executor.submit(this);
+	}
+
+	/**
+	 * Assures to shut down the Tracker thread
+	 */
+
+	public void stopTracker() {
+		stop = true;
+
+		executor.shutdownNow();
+	}
+
+	@Override
+	public void run() {
+		StopWatch stopWatch = new StopWatch();
+		while (true) {
+			if (Thread.currentThread().isInterrupted() || stop) {
+				logger.debug("Tracker stopping");
+				break;
+			}
+
+			List<User> users = tourGuideService.getAllUsers();
+			logger.debug("Begin Tracker. Tracking " + users.size() + " users.");
+			stopWatch.start();
+			users.forEach(u -> tourGuideService.trackUserLocation(u));
+			boolean notFinished = true;
+			while (notFinished) {
+				try { //
+					logger.debug("Waiting for tracking to finish...");
+					TimeUnit.MILLISECONDS.sleep(100);
+				} catch (InterruptedException e) {
+					break;
+				}
+
+				if (!completedTrackingMap.containsValue(false)) {
+					notFinished = false;
+				}
+			}
+
+			completedTrackingMap.clear();
+			stopWatch.stop();
+
+			logger.debug("Tracker Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
+			stopWatch.reset();
+			try {
+				logger.debug("Tracker sleeping");
+				TimeUnit.SECONDS.sleep(trackingPollingInterval);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+
+	}
+}
